bb2fa1dda249f3fc319e2f15f2ddd563aab78386
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-03 08:24:44 -0300

    group_anagrams solucao 1

--- group_anagrams/solution.py
+++ group_anagrams/solution.py
@@ -1,6 +1,16 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        pass
-    
-    def isAnagram(self, s, t):
-        return all(s.count(_) == t.count(_) for _ in 'abcdefghijklmnopqrstuvwxzy')
+
+        new_list = []
+        empty_dict ={}
+
+        for item in strs:
+            sorted_item = ''.join(sorted(item))
+            if sorted_item not in empty_dict:
+                empty_dict[sorted_item] = [item]
+            else:
+                empty_dict[sorted_item].append(item)
+        # return empty_dict 
+        for k,v in empty_dict.items():
+            new_list.append(v)
+        return new_list

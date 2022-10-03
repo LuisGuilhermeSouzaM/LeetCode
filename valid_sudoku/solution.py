75f06d81fbabd312a578db9332ba473e6e7583a6
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-03 10:37:44 -0300

    valid_sudoku less than 97% space

---
+++ valid_sudoku/solution.py
@@ -0,0 +1,59 @@
+class Solution(object):
+    
+    def notInRow(self, arr, row):
+        st = set()
+
+        for i in range(0, 9):
+            if arr[row][i] in st:
+                return False
+            if arr[row][i] != '.':
+                st.add(arr[row][i])
+
+        return True
+    def notInCol(self, arr, col):
+
+        st = set()
+
+        for i in range(0, 9):
+            if arr[i][col] in st:
+                return False
+
+            if arr[i][col] != '.':
+                st.add(arr[i][col])
+
+        return True
+
+    def notInBox(self, arr, startRow, startCol):
+
+        st = set()
+
+        for row in range(0, 3):
+            for col in range(0, 3):
+                curr = arr[row + startRow][col + startCol]
+
+                if curr in st:
+                    return False
+
+                if curr != '.':
+                    st.add(curr)
+
+        return True
+
+    def isValid(self, arr, row, col):
+        return (self.notInRow(arr, row) and self.notInCol(arr, col) and
+                self.notInBox(arr, row - row % 3, col - col % 3))
+
+
+    def isValidSudoku(self, arr):
+        """
+        Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+        """
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if not self.isValid(arr, i, j):
+                    return False
+        return True

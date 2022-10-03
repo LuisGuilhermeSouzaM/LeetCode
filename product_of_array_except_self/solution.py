8be7f51ee7e4f7915db6b11abca98af8b07c7814
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-03 09:41:58 -0300

    product_array_excep_self

---
+++ product_of_array_except_self/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
+        answer = [0] * len(nums)
+        prefix[0] = 1
+        suffix[len(nums) - 1] = 1
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        for i in range(len(nums)):
+            answer[i] = prefix[i] * suffix[i]
+        return answer

db8fe051bd5f12aed42c5d2187da723f77b7d80e
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-10-03 09:49:34 -0300

    product_of_array O(1) space complexity

--- product_of_array_except_self/test.py
+++ product_of_array_except_self/test.py
@@ -1,16 +1,11 @@
 def productExceptSelf(nums):
-    prefix = [1] * len(nums)
-    suffix = [1] * len(nums)
-    answer = [0] * len(nums)
-    prefix[0] = 1
-    suffix[len(nums) - 1] = 1
-    for i in range(1, len(nums)):
-        prefix[i] = prefix[i - 1] * nums[i - 1]
-    for i in range(len(nums) - 2, -1, -1):
-        suffix[i] = suffix[i + 1] * nums[i + 1]
-    for i in range(len(nums)):
-        answer[i] = prefix[i] * suffix[i]
-    return answer, suffix, prefix
+    n, ans, suffix_prod = len(nums), [1]*len(nums), 1
+    for i in range(1,n):
+        ans[i] = ans[i-1] * nums[i-1]
+    for i in range(n-1,-1,-1):
+        ans[i] *= suffix_prod
+        suffix_prod *= nums[i]
+    return ans
 if __name__ == "__main__":
     nums = [1,2,3,4]
     print(productExceptSelf(nums))
